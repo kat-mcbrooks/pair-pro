45624a0273d68c9969e4d9866e7a485e1b9ec048
kat-mcbrooks <katharinebrooks@hotmail.co.uk>
2022-03-02 16:15:03 +0000

    uncommented out cors.

--- server/server.js
+++ server/server.js
@@ -1,13 +1,15 @@
 const express = require("express");
 require("dotenv").config({ path: "./config.env" });
-// const cors = require("cors");
+const cors = require("cors");
 const { errorHandler } = require("./middleware/errorMiddleware");
 const connectDB = require("./db/conn");
 const port = process.env.PORT || 5000;
 
 const app = express();
 connectDB();
-// app.use(cors());
+
+app.use(cors()); //cors is needed for security apparently
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
