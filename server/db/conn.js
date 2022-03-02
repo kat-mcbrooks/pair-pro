e23f36843a437e5fd4bc2a79c9b61182c7f4f8dc
kat-mcbrooks <katharinebrooks@hotmail.co.uk>
2022-03-02 12:43:58 +0000

    added dependencies, including mongoose. Updated db/conn.js file to use mongoose for db connection. Server up and running ok

--- server/db/conn.js
+++ server/db/conn.js
@@ -1,26 +1,14 @@
-const { MongoClient } = require("mongodb");
-const Db = process.env.ATLAS_URI;
-const client = new MongoClient(Db, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
- 
-var _db;
- 
-module.exports = {
-  connectToServer: function (callback) {
-    client.connect(function (err, db) {
-      // Verify we got a good "db" object
-      if (db)
-      {
-        _db = db.db("PairPro");
-        console.log("Successfully connected to MongoDB."); 
-      }
-      return callback(err);
-         });
-  },
- 
-  getDb: function () {
-    return _db;
-  },
+const mongoose = require("mongoose");
+
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.ATLAS_URI);
+
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.log(error);
+    process.exit(1);
+  }
 };
+
+module.exports = connectDB;
--- server/server.js
+++ server/server.js
@@ -1,19 +1,20 @@
 const express = require("express");
-const app = express();
-const cors = require("cors");
 require("dotenv").config({ path: "./config.env" });
+const cors = require("cors");
+const { errorHandler } = require("./middleware/errorMiddleware");
+const connectDB = require("./db/conn");
 const port = process.env.PORT || 5000;
+
+connectDB();
+
+const app = express();
+
 app.use(cors());
 app.use(express.json());
-app.use(require("./routes/users"));
-// get driver connection
-const dbo = require("./db/conn");
- 
-app.listen(port, () => {
-  // perform a database connection when server starts
-  dbo.connectToServer(function (err) {
-    if (err) console.error(err);
- 
-  });
-  console.log(`Server is running on port: ${port}`);
-});
+app.use(express.urlencoded({ extended: false }));
+
+app.use("/api/users", require("./routes/users"));
+
+app.use(errorHandler);
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
