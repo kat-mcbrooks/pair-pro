fb054f10ff7ceae80f6cfecde7609e84ec148d2e
Vanessa Florio <vanessaflorio1997@gmail.com>
2022-03-05 17:43:56 +0000

    added procfile and path in server.js file

--- server/server.js
+++ server/server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const dotenv = require("dotenv").config();
+const dotenv = require("dotenv").config({ path: '.env'});
 const cors = require("cors");
 const { errorHandler } = require("./middleware/errorMiddleware");
 const connectDB = require("./config/db");
