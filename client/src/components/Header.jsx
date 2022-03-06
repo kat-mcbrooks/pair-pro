258e02ac8745c720173144ecdf7a36ed65841b15
kat-mcbrooks <katharinebrooks@hotmail.co.uk>
2022-03-06 18:28:57 +0000

    spacing amendments header

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,24 +1,12 @@
-
-
-
-
 import { useState, useContext } from "react";
 import { AuthContext } from "../App";
 import { FaSignInAlt, FaSignOutAlt, FaUser } from "react-icons/fa";
 import { Link, useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
 
-
 function Header() {
-  // read authcontext value from the context object using useContext Hook in the component that needs the authcontext value(in our case, Header)
-  // if the context value of the AuthContext.Provider(see app.js) changes, any component that calls the useContext will be rerendered with the latest value in the context provider.
   const { dispatch } = useContext(AuthContext);
   const { state } = useContext(AuthContext);
-
-  // then check the login state, reading it from
-  //const [loginStatus, setLoginStatus] = useState(state.isLoggedIn);
-
-  // const navigate = useNavigate();
 
   const logout = () => {
     localStorage.removeItem("userToken");
@@ -28,11 +16,9 @@ function Header() {
   };
 
   return (
-
     <header data-testid="nav" className="header">
       <div className="logo">
         <Link to="/">PairPro</Link>
-
       </div>
       <ul>
         {state.isLoggedIn ? (
@@ -56,7 +42,6 @@ function Header() {
           </>
         )}
       </ul>
-      
     </header>
   );
 }
