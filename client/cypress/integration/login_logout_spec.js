2556d7e365aa10327973ee3b84eed1f8dc4b0d8f
Vanessa Florio <vanessaflorio1997@gmail.com>
2022-03-09 15:03:14 +0000

    cypress: login, logout, and prairpros path

--- client/cypress/integration/login_logout_spec.js
+++ client/cypress/integration/login_logout_spec.js
@@ -5,6 +5,17 @@ describe('login', () => {
       cy.get('#password').type('password1');
       cy.get('button[type="submit"]')
       .should('be.visible')
-      .click()
+      .click()      
+  })
+  
+
+
+  it('brings user to /prospage path', () => {
+    cy.url()
+    .should('be.equal', 'http://localhost:3000/pairpros');
+  })
+
+  it('user can logout', () => {
+    cy.contains('Logout').click()
   })
 })
