8795bb838aba00bcf474ecc8b91353818b83b72c
Hannah Desmond <desmond.hannah@googlemail.com>
2022-03-09 18:16:58 +0000

    Update Profile Card component

--- client/src/components/ProfileCardSmall.jsx
+++ client/src/components/ProfileCardSmall.jsx
@@ -1,22 +1,14 @@
 import { Col, Card } from "react-bootstrap";
 import ChatToButton from "./ChatToButton";
-import robot from "../assets/robot.png";
-import person1 from "../assets/people/1.jpeg"
-import person2 from "../assets/people/2.jpeg"
-import person3 from "../assets/people/3.jpeg"
-import person4 from "../assets/people/4.jpeg"
-import person5 from "../assets/people/5.jpeg"
-import person6 from "../assets/people/6.jpeg"
+
 
 const ProfileCardSmall = ({person}) => {
-  const people = [
-    person1, person2, person3, person4, person5, person6, robot
-  ]
+
   return (
     <Col>
       <Card className="card" border="warning" >
       <Card.Header as="h5" bg="warning">{person.name}</Card.Header>
-        <Card.Img variant="top" src={people[Math.floor(Math.random()*people.length)]} />
+        <Card.Img variant="top" src={person.image} alt="profile picture"/>
         <Card.Body>
           <Card.Text>Languages: {person.languages}</Card.Text>
           <Card.Text>Bio: {person.bio}</Card.Text>
