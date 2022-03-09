7da8276c01aa85a6571ef5bbfc0cd0dda456ab6b
Vanessa Florio <vanessaflorio1997@gmail.com>
2022-03-09 10:10:58 +0000

    added socket script in package.json

--- server/server.js
+++ server/server.js
@@ -25,7 +25,7 @@ app.use(errorHandler);
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, '../client/build')));
-  app.use(express.static(path.join(__dirname, '../socket/build')));
+  app.use(express.static(path.join(__dirname, '../socket/start')));
 
   app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '../client', 'build', 'index.html'));
